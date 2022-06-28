2eb27d21799753c417c4cd8d0ecc8a4a013cfaf0
Matthew Bolger <xan1000@gmail.com>
2022-06-28 17:12:44 +1000

    - Refactored code extracting most logic to Soundboard class. - The sound file URL is now determined without hard-coding (the sb.js file is examined).

--- Program.cs
+++ Program.cs
@@ -1,7 +1,5 @@
 ﻿// Intended to be used with http://www.realmofdarkness.net/ and its various domains.
-
-using System.Text.RegularExpressions;
-using Newtonsoft.Json;
+using SoundboardRipper;
 
 // 1 argument is required which is the soundboard URL, for example: http://www.realmofdarkness.net/sb/sw-lightsaber/
 if(args.Length != 1)
@@ -9,7 +7,6 @@ if(args.Length != 1)
     Console.WriteLine("Must include the soundboard URL, for example: http://www.realmofdarkness.net/sb/sw-lightsaber/");
     return;
 }
-
 if(!Uri.TryCreate(args[0], UriKind.Absolute, out var url))
 {
     Console.WriteLine("URL is not valid.");
@@ -18,36 +15,23 @@ if(!Uri.TryCreate(args[0], UriKind.Absolute, out var url))
 
 using var client = new HttpClient();
 
-// Get the HTML of the page.
-var html = await client.GetStringAsync(url);
-var urlBuilder = new UriBuilder(url.Scheme, url.Host, url.Port); 
+var soundboard = new Soundboard(url, client);
 
-var soundsPath = Regex.Match(html, "<script.*src=\"(?<sounds>.*sounds.js)\".*<\\/script>").Groups["sounds"].Value;
-urlBuilder.Path = soundsPath;
-
-var soundsJavaScript = await client.GetStringAsync(urlBuilder.Uri);
-
-// Find the starting [ and use everything up until the ;.
-var soundsJson = soundsJavaScript[soundsJavaScript.IndexOf('[')..soundsJavaScript.IndexOf(';')];
-
-// ReSharper disable once AssignNullToNotNullAttribute
-var sounds = JsonConvert.DeserializeObject<List<string>>(soundsJson).Select(x => $"{x}.mp3").ToList();
-
-var lastSegement = url.Segments[^1];
-var folderPath = $"sounds/{lastSegement}";
+var soundFileUrls = await soundboard.GetSoundFileUrlsAsync();
+var folderPath = soundboard.GetFolderPath();
 
 // Create folder if it doesn't already exist.
 Directory.CreateDirectory(folderPath);
 
-Console.WriteLine($"Downloading {sounds.Count} sound files:");
-for(var i = 0; i < sounds.Count; i++)
+Console.WriteLine($"Downloading {soundFileUrls.Length} sound files:");
+for(var i = 0; i < soundFileUrls.Length; i++)
 {
-    var fileName = sounds[i];
-    Console.Write($"    File #{i + 1}. Downloading {fileName}... ");
+    var fileUrl = soundFileUrls[i];
+    var fileName = soundboard.GetFileName(fileUrl);
+    Console.Write($"    #{i + 1}. {fileName} ... ");
 
-    urlBuilder.Path = $"/audio/{lastSegement.Replace('-', '/')}{fileName}";
-    var soundData = await client.GetByteArrayAsync(urlBuilder.Uri);
-    await File.WriteAllBytesAsync(folderPath + fileName, soundData);
+    var soundData = await client.GetByteArrayAsync(fileUrl);
+    await File.WriteAllBytesAsync(Path.Join(folderPath + fileName), soundData);
 
     Console.WriteLine("done.");
 }
---
+++ Soundboard.cs
@@ -0,0 +1,68 @@
+﻿using System.Text.RegularExpressions;
+using Newtonsoft.Json;
+
+namespace SoundboardRipper;
+
+public class Soundboard
+{
+    private readonly Uri _url;
+    private readonly HttpClient _client;
+    private readonly UriBuilder _urlBuilder;
+
+    public Soundboard(Uri url, HttpClient client)
+    {
+        _url = url;
+        _client = client;
+        _urlBuilder = new UriBuilder(url.Scheme, url.Host, url.Port);
+    }
+
+    public string GetFolderPath() => $"sounds/{_url.Segments[^1]}";
+
+    public string GetFileName(Uri soundFileUrl) => soundFileUrl.Segments[^1];
+    
+    public async Task<Uri[]> GetSoundFileUrlsAsync()
+    {
+        // Get the HTML of the page.
+        var html = await _client.GetStringAsync(_url);
+
+        var soundboard = await GetSoundboardFolderPathAndFileExtensionAsync(html);
+        var soundFileNames = await GetSoundFileNamesAsync(html);
+
+        return soundFileNames.Select(fileName =>
+        {
+            _urlBuilder.Path = soundboard.folderPath + fileName + soundboard.fileExtension;
+
+            return _urlBuilder.Uri;
+        }).ToArray();
+    }
+
+    private async Task<string[]> GetSoundFileNamesAsync(string html)
+    {
+        // Retrieve the sound file names from the sounds.js file.
+        _urlBuilder.Path =
+            Regex.Match(html, "<script.*src=\"(?<sounds>.*sounds.js)\".*<\\/script>").Groups["sounds"].Value;
+
+        var soundsJavaScript = await _client.GetStringAsync(_urlBuilder.Uri);
+
+        // Find the starting [ and use everything up until the ;.
+        var soundsJson = soundsJavaScript[soundsJavaScript.IndexOf('[')..soundsJavaScript.IndexOf(';')];
+
+        return JsonConvert.DeserializeObject<string[]>(soundsJson);
+    }
+
+    private async Task<(string folderPath, string fileExtension)> GetSoundboardFolderPathAndFileExtensionAsync(
+        string html)
+    {
+        // Retrieve the soundboard sb.js file to extract the folder path and file extension to build the URL for the
+        // sound files.
+        _urlBuilder.Path =
+            Regex.Match(html, "<script.*src=\"(?<soundboard>.*sb.js)\".*<\\/script>").Groups["soundboard"].Value;
+
+        var soundboardJavaScript = await _client.GetStringAsync(_urlBuilder.Uri);
+
+        // Extract the folder path and file extension from the javascript.
+        var match = Regex.Match(soundboardJavaScript, "src:.*\\[\"(?<folderPath>.*)\".*\"(?<fileExtension>.*)\"]");
+
+        return (match.Groups["folderPath"].Value, match.Groups["fileExtension"].Value);
+    }
+}
